454eb4e925fb1a6a8a21fd0eea5aa060524ec7d3
diego beltran <Diicrowork@gmail.com>
2024-09-25 01:43:31 -0500

    Imports que me faltaron

--- src/controller/modos/products.modos.js
+++ src/controller/modos/products.modos.js
@@ -1,6 +1,7 @@
 import productsModel from "../../dao/models/products.model.js";
 import CustomError from "../customError.js";
 import { errorDicctionary } from "../errorsDictionary.js";
+import { transport } from "../utils.js";
 import CollectionManager from "./modos.manager.js";
 
 class productDTO {
