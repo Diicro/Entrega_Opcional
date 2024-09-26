8d282485d697deeec8cb173784f8fe391fb35855
diego beltran <Diicrowork@gmail.com>
2024-09-25 20:55:17 -0500

    se disminuye la eliminación de cuenta por inactividad

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -119,23 +119,24 @@ console.log(actualDate)
 
         const diferenceTime = new Date() - last_connectionUser ;
         console.log(diferenceTime);
-        if (diferenceTime / 1800000 > 1) {
-      console.log("entras 3s")
+        if (diferenceTime / 900000 > 1) {
 
-          await userModel.deleteOne({ email: element.email });
+      console.log(diferenceTime/900000)
+
           transport.sendMail({
             from: `FachaPets <${config.GMAIL_APP_USER}>`,
             to: element.email,
             subject: `Cuenta Eliminada`,
             hatml: `<h1>Aviso Cuenta eliminada</h1><div>Su cuenta ha sido eliminada debido a la inactividad,gracias por elegirnos.</div>`,
           });
+          await userModel.deleteOne({ email: element.email });
           users++;
         }
       });
       res
         .status(200)
         .send(
-          `Se eliminaron: ${users} usuarios por inactividad mayor a 30 min`
+          `Se eliminaron: ${users} usuarios por inactividad mayor a 15 min`
         );
     } catch (error) {
       next(new CustomError(errorDicctionary.DATABASE_ERROR));
